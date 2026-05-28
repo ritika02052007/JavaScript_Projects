55e36ca653aaabf24981f3699d99beda51ceba87
ritika02052007 <ritika02052007@gmail.com>
2026-05-28 20:33:17 +0530

    add something

--- BackGround_changer/main.js
+++ BackGround_changer/main.js
@@ -1,28 +1,28 @@
-const buttons = document.querySelectorAll(".button")
-const body = document.querySelector('body')
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
 
-buttons.forEach( function (button) {
-    console.log(button);
-    button.addEventListener("click" , function (e) {
-        console.log(e);
-        console.log(e.target);
-        if(e.target.id === "sienna"){
-            body.style.backgroundColor = e.target.id
-        }
-         if(e.target.id === "coral"){
-            body.style.backgroundColor = e.target.id
-        }
-         if(e.target.id === "orange"){
-            body.style.backgroundColor = e.target.id
-        }
-         if(e.target.id === "olive"){
-            body.style.backgroundColor = e.target.id
-        }
-         if(e.target.id === "red"){
-            body.style.backgroundColor = e.target.id
-        }
-         if(e.target.id === "magenta"){
-            body.style.backgroundColor = e.target.id
-        }
-    })
-})
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "sienna") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "coral") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "orange") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "olive") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "red") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "magenta") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
